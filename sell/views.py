d78746239de3d4a2c342a8088d96ed462185b415
Alec <hsu022210@gmail.com>
2016-05-08 13:01:47 +0800

    change portfolio img

--- sell/views.py
+++ sell/views.py
@@ -5,6 +5,6 @@ from .models import Item
 
 
 def index(request):
-    items = Item.objects.all()
+    items = Item.objects.all().order_by('-posted')
     context = {'items': items, }
     return render(request, 'sell/sell.html', context)
