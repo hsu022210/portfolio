ed7f7d07b864156072abfcce6fb436a927fe5544
Alec <hsu022210@gmail.com>
2016-05-10 19:46:05 +0800

    display success message while contact

--- contact/views.py
+++ contact/views.py
@@ -2,13 +2,15 @@
 from django.core.urlresolvers import reverse_lazy
 from .forms import ContactForm
 from django.views.generic.edit import FormView
+from django.contrib.messages.views import SuccessMessageMixin
 
 # Create your views here.
 
 
-class ContactView(FormView):
+class ContactView(SuccessMessageMixin, FormView):
     template_name = 'contact/contact.html'
     success_url = reverse_lazy('contact')
+    success_message = "Your form has submitted successfully"
     form_class = ContactForm
 
     def form_valid(self, form):
